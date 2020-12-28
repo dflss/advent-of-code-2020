f56f187bfff487c7f064df0a5c1efe94e0ab56fe
Dominika Florczykowska <dflorczy@protonmail.com>
2020-12-28 17:43:31 +0000

    Add day 22

--- day22.py
+++ day22.py
@@ -28,17 +28,17 @@ def part_1():
 
 
 def play(deck1, deck2):
-    states = []
+    states = set()
     while not (len(deck1) == 0 or len(deck2) == 0):
-        if (deck1, deck2) in states:
-            return deck1, deck2, 1
-        states.append((deck1.copy(), deck2.copy()))
+        if (state := str((deck1, deck2))) in states:
+            return 1, deck1
+        states.add(state)
         card1 = deck1.popleft()
         card2 = deck2.popleft()
         if len(deck1) >= card1 and len(deck2) >= card2:
             subdeck1 = deque(islice(deck1, 0, card1))
             subdeck2 = deque(islice(deck2, 0, card2))
-            d1, d2, winner = play(subdeck1, subdeck2)
+            winner, _ = play(subdeck1, subdeck2)
             if winner == 1:
                 deck1.append(card1)
                 deck1.append(card2)
@@ -46,23 +46,21 @@ def play(deck1, deck2):
                 deck2.append(card2)
                 deck2.append(card1)
         else:
-            # print(f"play standard combat: {card1}, {card2}")
             if card1 > card2:
                 deck1.append(card1)
                 deck1.append(card2)
             else:
                 deck2.append(card2)
                 deck2.append(card1)
-    return deck1, deck2, 1 if len(deck1) > 0 else 2
+    return (1, deck1) if len(deck1) > 0 else (2, deck2)
 
 
 def part_2():
     input = parse_input_file()
     deck1 = deque([int(i) for i in input[0].splitlines()[1:]])
     deck2 = deque([int(i) for i in input[1].splitlines()[1:]])
-    d1, d2, winner = play(deck1, deck2)
+    winner, winning_deck = play(deck1, deck2)
     score = 0
-    winning_deck = deck1 if winner == 1 > 0 else deck2
     for i, card in enumerate(reversed(winning_deck)):
         score += (i + 1) * card
     print(score)
